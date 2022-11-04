d6daa92ac936390335e95dc2f5708c21ffd97efc
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-11-04 15:08:31 -0300

    exercicio 5 feito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { getUser } from '../services/userAPI';
 import Carregando from './Carregando';
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
   state = {
@@ -19,13 +20,16 @@ export default class Header extends Component {
     const { name, isLoading } = this.state;
     return (
       <header data-testid="header-component">
-        {
-          isLoading ? (
-            <h1 data-testid="header-user-name">{name}</h1>
-          ) : (
-            <Carregando />
-          )
-        }
+        { isLoading ? (
+          <h1 data-testid="header-user-name">{name}</h1>
+        ) : (
+          <Carregando />
+        )}
+        <ul>
+          <Link to="/search" data-testid="link-to-search"> Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+        </ul>
       </header>
     );
   }
