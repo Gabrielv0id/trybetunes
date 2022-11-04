1735a3206adb9879f21daaa77bbfdc877d2d6226
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-11-04 19:32:06 -0300

    exercicio 6 feito

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Carregando from '../components/Carregando';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 export default class Search extends Component {
   state = {
     artist: '',
+    nameArtist: '',
     isButtonDisable: true,
+    isLoading: false,
+    isButtonClicked: false,
+    albums: [],
   };
 
   onInputChange = (event) => {
@@ -30,8 +37,30 @@ export default class Search extends Component {
     );
   };
 
+  onButtonClick = async (event) => {
+    const { artist } = this.state;
+    event.preventDefault();
+    this.setState({ isLoading: true });
+    const response = await searchAlbumsAPI(artist);
+    console.log(response);
+    this.setState({
+      nameArtist: artist,
+      albums: response,
+      isLoading: false,
+      isButtonClicked: true,
+      artist: '',
+    });
+  };
+
   render() {
-    const { artist, isButtonDisable } = this.state;
+    const {
+      artist,
+      isButtonDisable,
+      isLoading,
+      isButtonClicked,
+      albums,
+      nameArtist,
+    } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
@@ -46,14 +75,41 @@ export default class Search extends Component {
               onChange={ this.onInputChange }
             />
             <button
-              type="button"
+              type="submit"
               data-testid="search-artist-button"
               disabled={ isButtonDisable }
+              onClick={ this.onButtonClick }
             >
               Pesquisar
             </button>
           </label>
         </form>
+        {isLoading && <Carregando />}
+        {(isButtonClicked && albums.length > 0)
+         && (
+           <>
+             <h2>{`Resultado de álbuns de: ${nameArtist} `}</h2>
+             <ul>
+               {albums.map((album) => (
+                 <li key={ album.collectionName }>
+                   <Link
+                     to={ `/album/${album.collectionId}` }
+                     data-testid={ `link-to-album-${album.collectionId}` }
+                   >
+                     <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+                   </Link>
+                   <h4>{album.collectionName}</h4>
+                   <p>{album.artistName}</p>
+                 </li>))}
+             </ul>
+           </>
+         )}
+        {(isButtonClicked && albums.length === 0)
+          && (
+            <>
+              <h2>{`Resultado de álbuns de: ${nameArtist} `}</h2>
+              <h3>Nenhum álbum foi encontrado</h3>
+            </>)}
       </div>
     );
   }
