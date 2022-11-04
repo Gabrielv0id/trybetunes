35359c78ac8194e55da6121124071e162422d364
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-11-04 15:34:32 -0300

    exercicio 5 feito

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,58 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    artist: '',
+    isButtonDisable: true,
+  };
+
+  onInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState(
+      {
+        [name]: value,
+      },
+      () => {
+        this.valideButton();
+      },
+    );
+  };
+
+  valideButton = () => {
+    const { artist } = this.state;
+    const minimum = 2;
+    const isValid = artist.length < minimum;
+    this.setState(
+      {
+        isButtonDisable: isValid,
+      },
+    );
+  };
+
   render() {
+    const { artist, isButtonDisable } = this.state;
     return (
       <div data-testid="page-search">
-        Search
         <Header />
+        <form>
+          <label htmlFor="name">
+            <input
+              type="text"
+              name="artist"
+              id="name"
+              data-testid="search-artist-input"
+              value={ artist }
+              onChange={ this.onInputChange }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ isButtonDisable }
+            >
+              Pesquisar
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
